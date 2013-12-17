a81c55303c9403be0eadcf664adffe4d4b3fa3ec
Dieter Reuter <dieter.reuter@me.com>
2013-12-17 18:26:24 +0100

    Changed file start-watchman.sh, Auto-Commit V0.1

--- start-watchman.sh
+++ start-watchman.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/projects/watchman/watchman -- trigger ~/projects/git-autocommit auto-commit '*' -- ./auto-commit.sh
+watchman -- trigger ~/projects/git-autocommit auto-commit '*' -- ./auto-commit.sh
 
